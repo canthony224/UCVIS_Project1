4fb1bb4c61550611be24835c9248ba861ea54353
canthony224 <calebjanthony26@gmail.com>
2023-02-26 22:29:25 -0500

    Added more functionality to bar charts

--- js/barchart.js
+++ js/barchart.js
@@ -10,7 +10,7 @@ class BarChart {
         parentElement: _config.parentElement,
         containerWidth: _config.containerWidth || 600,
         containerHeight: _config.containerHeight || 400,
-        margin: _config.margin || {top: 25, right: 20, bottom: 20, left: 35},
+        margin: _config.margin || {top: 50, right: 30, bottom: 50, left: 45},
         tooltipPadding: _config.tooltipPadding || 15
       }
       this.data = _data;
@@ -20,6 +20,8 @@ class BarChart {
       this.xLabelText = _config.xLabelText
       this.yLabelText = _config.yLabelText
       this.scale = _config.scale
+      this.title = _config.title
+      this.showHabit = _config.showHabit
       this.initVis();
     }
 
@@ -29,35 +31,45 @@ class BarChart {
     this.width = this.config.containerWidth - this.config.margin.left - this.config.margin.right;
     this.height = this.config.containerHeight - this.config.margin.top - this.config.margin.bottom;
 
-    console.log("generating bar chart",this.data)
-   
-  
+    console.log("generating bar chart",this.data)  
     
     
     // Construct scales, axes, and formats.
     if (this.scale == 'time'){
-      const xDomain = d3.extent(this.data,d=>d[this.xLabel])
-      const xRange = [this.width/this.data.length/2, this.width-this.width/this.data.length/2]
+      const xDomain = d3.extent(this.data,d => { return new Date(d[0], 0, 1)})
+      const xRange = [this.width/this.data.size/2, this.width-this.width/this.data.size/2]
       this.xScale = d3.scaleTime(xDomain, xRange)
       console.log(xDomain,xRange)
     }else if (this.scale == 'category'){      
-      const xDomain = d3.map(this.data,function(d) {return d.xLabel; });
+      const xDomain = d3.map(this.data,function(d) {return d[0]; });
       const xRange = [0, this.width]
       this.xScale = d3.scaleBand(xDomain,xRange)
       console.log(xDomain,xRange)
+      console.log(this.xScale('G'),this.xScale('F'))
     }
 
     //.domain(xDomain)
     //.range(xRange)
     //const xzScale = d3.scaleBand(zDomain, [0, xScale.bandwidth()]).padding(zPadding);
-    const yDomain = [0,d3.max(this.data,d=>d[this.yLabel])] // posibly reverse??
-    const yRange = [this.height,0]
-    this.yScale = d3.scaleLinear(yDomain, yRange);
+    let yDomain = [0,d3.max(this.data,d=>{
+      // if self.showHabit??
+      
+      return d[1].get('total')
+    })] // posibly reverse??
+    let yRange = [this.height,0]
+    if (yDomain[1] - yDomain[0] > 1000) { // if obver 1k difference switch to log scale
+        yDomain[0] = 1
+        this.yScale = d3.scaleLog(yDomain, yRange)
+    }else{
+        this.yScale = d3.scaleLinear(yDomain, yRange).nice();
+    }
+    console.log('Ydom',yDomain,yRange)
+    
     //const zScale = d3.scaleOrdinal(zDomain, colors);
-    this.xAxis = d3.axisBottom(this.xScale);
-    //.ticks()
+    this.xAxis = d3.axisBottom(this.xScale)
+    //.tickFormat();
     this.yAxis = d3.axisLeft(this.yScale)
-    //.ticks()
+    .ticks(null,"~s")
   
     this.svg = d3.select(this.config.parentElement)
         .attr('width', this.config.containerWidth)
@@ -80,7 +92,7 @@ class BarChart {
     // Append both axis titles
     this.chart.append('text')
         .attr('class', 'axis-title')
-        .attr('y', this.height +15)
+        .attr('y', this.height +40)
         .attr('x', this.width + 30)
         .attr('dy', '.71em')
         .style('text-anchor', 'end')
@@ -88,36 +100,55 @@ class BarChart {
 
     this.svg.append('text')
         .attr('class', 'axis-title')
-        .attr('x', 0)
-        .attr('y', 0)
+        .attr('x', 1)
+        .attr('y', 15)
         .attr('dy', '.71em')
         .text(this.yLabelText);
+
+
+    // chart title
+    this.svg.append("text")
+    .attr("x", this.width / 2 )
+    .attr("y", 11)
+    .style("text-anchor", "middle")
+    .text("Title of Graph");
   }
 
   updateVis(){
-
+    // TODO: make mouse change onhover with elements to click
     this.bars = this.chart.selectAll('.bar')
     .data(this.data)
     .join("rect")
       .attr('class', 'bar')
       .attr("x", d => {
-          //console.log(this.xScale())
         //console.log(this.xScale(d[this.xLabel]))
         if (this.scale == 'time'){
-          return this.xScale(d[this.xLabel]) - (this.width/this.data.length/2) +2; 
+          let xDate = new Date(d[0], 0, 1)
+          return this.xScale(xDate) - (this.width/this.data.size/2) +2; 
         }else if (this.scale == 'category'){
-          console.log("bandscale",d[this.xLabel],this.xScale.bandwidth(d[this.xLabel]))
-          return this.xScale.bandwidth(d[this.xLabel]); 
+          //console.log("bandscale",d[this.xLabel],this.xScale(d[this.xLabel]))
+          return this.xScale(d[0]); 
         } 
         //return this.xScale(d[this.xLabel]) -2
       })
       .attr("y", d => {
-        //
-        return this.yScale(d[this.yLabel])})
-      .attr("width", d => {
-        return this.width / this.data.length - 4
+        if (this.showHabit){ //TODO: make this a tripple stacked bar
+          let totalHeight = d[1].get('total')
+          return this.yScale(totalHeight)
+        }else{
+          let totalHeight = d[1].get('total')
+          return this.yScale(totalHeight)
+        }
       })
-      .attr("height", d => this.height - this.yScale(d[this.yLabel]))
+      .attr("width", d => {
+        return this.width / this.data.size - 4
+      })
+      .attr("height", (d) => {
+        let totalHeight = d[1].get('total')
+        let scaledHeight = this.height - this.yScale(totalHeight)
+        return scaledHeight
+      })
+        
       .attr("fill", d => {
         return '#00000'
       } ); //color??
@@ -125,25 +156,30 @@ class BarChart {
   
       this.bars
       .on('mouseover', (event,d) => {
-        console.log("mouse",d[this.xLabel],d[this.yLabel])
+        //console.log("mouse",d[this.xLabel],d[this.yLabel])
         let xOutput;
         let yOutput;
         if (this.scale == 'time'){
-          xOutput = d[this.xLabel].getFullYear();
+          xOutput = d[0];
         } else{ // just category for now
-          xOutput = d[this.xLabel]
+          xOutput = d[0]
         }
-        
-        yOutput = d[this.yLabel];
-        console.log("tip",xOutput,yOutput);
+
+        yOutput = d[1].get('total');
+        if (this.showHabit){
+
+        } else{
+          yOutput = d[1].get('total');
+        }
+        console.log(d[1],yOutput)
 
         d3.select('#tooltip')
           .style('display', 'block')
           .style('left', (event.pageX + this.config.tooltipPadding) + 'px')   
           .style('top', (event.pageY + this.config.tooltipPadding) + 'px')
           .html(`
-            <div class="tooltip-title">${xOutput}</div>
-            <div>Number of Exoplanets: ${yOutput}</div>
+            <div><b>${this.xLabelText}:</b> ${xOutput}</div>
+            <div><b>Number of ${this.yLabelText}:</b> ${yOutput}</div>
           `);
       })
       .on('mouseleave', () => {
@@ -152,11 +188,17 @@ class BarChart {
 
     this.xAxisG
     .call(this.xAxis)
+    .selectAll('text')
+    .attr("transform", (text) => {
+      if (text && text.length > 5){
+        return "rotate(-10)"
+      } 
+    })
     .call(g => g.select('.domain').remove());
 
     this.yAxisG
     .call(this.yAxis)
-    .call(g => g.select('.domain').remove())
+    
 
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,85 +1,12 @@
 /**
  * Load data from CSV file asynchronously and render scatter plot
  */
-let data, scatterplot, barchart;
+let data, scatterplot, barchart, viewHabitable;
 d3.csv('data/exoplanets.csv')
   .then(_data => {
     data = _data;
     
-    // Convert year into data
-    
-
-
-    sortedData = data.sort((a, b) => d3.ascending(a.disc_year, b.disc_year));
-    
-    let groupByYearDate = d3.rollup(
-      sortedData,
-      (group) => group.length, // grab count of group?
-      (label) => label.disc_year, // what to group them by?
-    );
-
-    groupByYearDate = Array.from(groupByYearDate, ([year, count]) => ({ year, count })); // converts rollup data to object array
-    groupByYearDate.forEach(d => { // convert year to date
-    d.year = new Date(d.year, 0, 1)
-    });
-    //console.log("grouped",groupByYearDate)
-
-    discoveryPerYear = new BarChart({
-      'parentElement': '#discVis',
-      'type': 'single',
-      'xLabel': 'year',
-      'yLabel': 'count',
-      'xLabelText': 'Year',
-      'yLabelText': 'Discoveries',
-      'scale': 'time',
-      'containerWidth': 900,
-      'containerHeight': 500
-    },groupByYearDate)
-
-    //sortedData = data.sort((a, b) => d3.ascending(a.disc_year, b.disc_year));
-    
-    let groupByStarCount = d3.rollup(
-      data,
-      (group) => group.length, // grab count of group?
-      (label) => label.sy_snum, // what to group them by?
-    );
-    groupByStarCount = Array.from(groupByStarCount, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
-
-    planetsPerStar = new BarChart({
-      'parentElement': '#starCount',
-      'type': 'single',
-      'xLabel': 'xLabel',
-      'yLabel': 'count',
-      'xLabelText': 'Number of stars',
-      'yLabelText': 'Exoplanets',
-      'scale': 'category',
-      'containerWidth': 900,
-      'containerHeight': 500
-    },groupByStarCount)
-
-
-    let groupByPlanetCount = d3.rollup(
-      data,
-      (group) => group.length, // grab count of group
-      (label) => label.sy_pnum, // what to group them by
-    );
-    groupByPlanetCount = Array.from(groupByPlanetCount, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
-
-    planetsPerPlanet = new BarChart({
-      'parentElement': '#planetCount',
-      'type': 'single',
-      'xLabel': 'xLabel',
-      'yLabel': 'count',
-      'xLabelText': 'Number of Planets',
-      'yLabelText': 'Exoplanets',
-      'scale': 'category',
-      'containerWidth': 900,
-      'containerHeight': 500
-    },groupByPlanetCount)
-
-
-    //(Format spectral type to just the first letter type:
-    //https://en.wikipedia.org/wiki/Stellar_classification <- how to classify
+    // preprocess and get starCategories
     const starCategories = ['A','F','G','K','M'];
     data.forEach(d => { // convert year to date
       let starType = Array.from(d.st_spectype)[0];
@@ -94,14 +21,114 @@ d3.csv('data/exoplanets.csv')
         d.starType = "Unknown";
       }
       });
+
+
+    // function to get habitable from inhabitabel
+    // 1 parsec = 206266.3 AU
+    // conversion = x * 
+    const habitableRanges = [[8.5,12.5],[1.5,2.2],[0.95,1.4],[0.38,0.56],[0.08,0.12]] // ranges for hability
+    data.forEach(d => {
+      let distanceFromStar = d['pl_orbsmax'] //in au
+      if (d.starType != "Unknown"){
+        let rangeCheck = habitableRanges[starCategories.indexOf(d.starType)]
+        if (distanceFromStar >= rangeCheck[0] && distanceFromStar <= rangeCheck[1]){
+          d.isHabitable = "1"
+          //console.log("habitable!",d.starType,rangeCheck,distanceFromStar,d.sy_dist)
+        }else{
+          d.isHabitable = '0'
+        }
+      } else{
+        d.isHabitable = "-1" //== unkown flag
+      }
+    })
+    // Convert year into data
+    function mapOutDataHabitable(data,column){ // counts groups of data (auto totals habitable)
+      let outputMap = d3.rollup(
+        data,
+        (group) => group.length, // grab count of group?
+        (label) => label[column], // what to group them by?
+        (habit) => habit.isHabitable // Counts types of inhabitables
+      );
+      // gets and totals habitable
+      outputMap.forEach((d,i) => { // convert year to date
+        let total = 0
+        d.forEach(element => {
+          total += element;
+        });
+        d.set('total',total)
+      });
+      return outputMap
+    }
     
-    const typeFiltered = data.filter(d => d.starType != "Unknown"); // filter out unkown star types for now
-    let groupByStarType = d3.rollup(
-      typeFiltered,
-      (group) => group.length, // grab count of group
-      (label) => label.starType, // what to group them by
-    );
-    groupByStarType = Array.from(groupByStarType, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
+    
+    const viewHabitable = true;
+
+    
+    // convert to date and sort
+    data.forEach((d) => { // convert year to date
+      d.yearDate = new Date(d.disc_year, 0, 1)
+    });
+    
+    sortedData = data.sort((a, b) => d3.ascending(a.disc_year, b.disc_year)); // sort by year first
+    let groupByYearDate = mapOutDataHabitable(sortedData,'disc_year')
+    //console.log('gotgroup',groupByYearDate)
+    discoveryPerYear = new BarChart({
+      'parentElement': '#discVis',
+      'type': 'single',
+      'xLabel': 'year',
+      'yLabel': 'count',
+      'xLabelText': 'Year',
+      'yLabelText': 'Discoveries',
+      'scale': 'time',
+      'containerWidth': 900,
+      'containerHeight': 500,
+      'showHabit': false
+    },groupByYearDate)
+
+    sortedData = data.sort((a, b) => d3.ascending(a.sy_snum, b.sy_snum)); // Can make dynamic
+    let groupByStarCount = mapOutDataHabitable(sortedData,'sy_snum')
+    //console.log(groupByStarCount)
+    //groupByStarCount = Array.from(groupByStarCount, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
+
+    planetsPerStar = new BarChart({
+      'parentElement': '#starCount',
+      'type': 'single',
+      'xLabel': 'xLabel',
+      'yLabel': 'count',
+      'xLabelText': 'Number of stars',
+      'yLabelText': 'Exoplanets',
+      'scale': 'category',
+      'containerWidth': 900,
+      'containerHeight': 500,
+      'showHabit': true
+    },groupByStarCount)
+
+    
+    sortedData = data.sort((a, b) => d3.ascending(a.sy_snum, b.sy_pnum));
+    let groupByPlanetCount = mapOutDataHabitable(data,'sy_pnum')
+    //groupByPlanetCount = Array.from(groupByPlanetCount, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
+
+    planetsPerPlanet = new BarChart({
+      'parentElement': '#planetCount',
+      'type': 'single',
+      'xLabel': 'xLabel',
+      'yLabel': 'count',
+      'xLabelText': 'Number of Planets',
+      'yLabelText': 'Exoplanets',
+      'scale': 'category',
+      'containerWidth': 900,
+      'containerHeight': 500,
+      'showHabit': true
+    },groupByPlanetCount)
+
+
+    //(Format spectral type to just the first letter type:
+    //https://en.wikipedia.org/wiki/Stellar_classification <- how to classif
+    
+    let typeFiltered = data.filter(d => d.starType != "Unknown"); // filter out unkown star types for now
+  
+    let groupByStarType = mapOutDataHabitable(typeFiltered,'starType')
+    //groupByStarType = Array.from(groupByStarType, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
 
     planetsPerStarType = new BarChart({
       'parentElement': '#starTypeCount',
@@ -112,16 +139,13 @@ d3.csv('data/exoplanets.csv')
       'yLabelText': 'Exoplanets',
       'scale': 'category',
       'containerWidth': 900,
-      'containerHeight': 500
+      'containerHeight': 500,
+      'showHabit': false
     },groupByStarType)
 
     
-    let groupByDiscMethod = d3.rollup(
-      data,
-      (group) => group.length, // grab count of group
-      (label) => label.discoverymethod, // what to group them by
-    );
-    groupByDiscMethod = Array.from(groupByDiscMethod, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
+    let groupByDiscMethod = mapOutDataHabitable(data,'discoverymethod')
+    //groupByDiscMethod = Array.from(groupByDiscMethod, ([xLabel, count]) => ({ xLabel, count })); // converts rollup data to object array
     planetsPerDiscMethod = new BarChart({
       'parentElement': '#discoveryMethodCount',
       'type': 'single',
@@ -131,7 +155,8 @@ d3.csv('data/exoplanets.csv')
       'yLabelText': 'Exoplanets',
       'scale': 'category',
       'containerWidth': 900,
-      'containerHeight': 500
+      'containerHeight': 500,
+      'showHabit': true
     },groupByDiscMethod)
 
 
